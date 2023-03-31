ba2914cd8ed4ea05eac950cc4a571625dfdb424e
Baptiste <hello@strack.dev>
2023-03-31 15:59:13 +0300

    chore: react-native app with basic VESDK implementation

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+import {VESDK, VideoEditorModal} from 'react-native-videoeditorsdk';
+
+VESDK.unlockWithLicense(require('./config/imgly/vesdk_license'));
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <VideoEditorModal
+        visible={true}
+        video={require('./assets/video_sample.mp4')}
+        onExport={result => console.log(result)}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
